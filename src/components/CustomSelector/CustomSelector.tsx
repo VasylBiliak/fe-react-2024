8f1edcf4be2d592d6f9e49d891b6755cf79dc515
VasylBiliak <https://github.com/VasylBiliak>
2024-05-21 18:38:49 +0300

    fixed the styles for the custom selector. LINTER ERROr.

--- src/components/CustomSelector/CustomSelector.tsx
+++ src/components/CustomSelector/CustomSelector.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import Icon_arrow_up from '@/assets/products/Ikon_arrow_up.svg';
+import IconArrowUp from '@/assets/products/Ikon_arrow_up.svg';
 import type { Selector } from '@/interfaces/Selector';
 
 import styles from './CustomSelector.module.css';
@@ -41,11 +41,7 @@ function CustomSelector() {
             <menu className={`${styles.selector__menu} ${isOpen ? styles.selector__open : ''}`}>
                 <button className={styles.selector__btn} onClick={() => setIsOpen((previous) => !previous)}>
                     {currentSelector.title}
-                    <img
-                        src={Icon_arrow_up}
-                        className={isOpen ? styles.selector__icon_up : styles.selector__icon_down}
-                        alt="Icon arrow up"
-                    />
+                    <img src={IconArrowUp} className={isOpen ? styles.selector__icon_up : styles.selector__icon_down} alt="Icon arrow up" />
                 </button>
                 <div className={styles.selector__drop}>
                     {menuSelectors.map((item) => (
